a5fd8c28581365ed3639a67a1fb99d1e415c7323
Kuruyia <8174691+Kuruyia@users.noreply.github.com>
2024-02-22 21:29:22 +0100

    chore: bootstrap the project
    This bootstraps the project by adding the Lumper VMM as the VMM for this
    serverless runtime.
    
    Only minor modifications were made to Lumper:
    - The bin and lib crates were merged into one
    - Dependencies were updated, and some code has been modified accordingly
    - The `tracing` library replaces raw calls to `println!`
    - The VMM was renamed to `cloudlet-vmm`
    
    A virtual workspace was also set up at the root of the repository.
    
    Co-authored-by: Lapsus Ordonné <22498591+lapsus-ord@users.noreply.github.com>
    Signed-off-by: Kuruyia <8174691+kuruyia@users.noreply.github.com>
    Signed-off-by: Lapsus Ordonné <22498591+lapsus-ord@users.noreply.github.com>

---
+++ src/vmm/src/args.rs
@@ -0,0 +1,26 @@
+//! Command-line arguments.
+use std::path::PathBuf;
+
+use clap::Parser;
+use clap_verbosity_flag::{InfoLevel, Verbosity};
+
+/// The Virtual Machine Manager for the Cloudlet serverless runtime.
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+pub struct CliArguments {
+    /// Path to the image of the Linux kernel to boot.
+    #[arg(short, long, env)]
+    pub kernel: PathBuf,
+
+    /// Number of virtual CPUs assigned to the guest.
+    #[clap(short, long, env, default_value = "1")]
+    pub cpus: u8,
+
+    /// Memory amount (in MBytes) assigned to the guest.
+    #[clap(short, long, env, default_value = "512")]
+    pub memory: u32,
+
+    /// Verbosity level.
+    #[command(flatten)]
+    pub verbose: Verbosity<InfoLevel>,
+}
