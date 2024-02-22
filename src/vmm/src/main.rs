f34916371f1aee82608b1472280dbe4267f4d7a7
Kuruyia <8174691+Kuruyia@users.noreply.github.com>
2024-02-22 22:44:30 +0100

    ci: add Rust CI
    This adds a basic CI pipeline that is triggered whenever there is a push
    or a pull request opened on the main branch.
    
    For now, this pipeline only builds the packages, checks their formatting
    using `cargo fmt` and their linting using `cargo clippy`.
    
    Signed-off-by: Kuruyia <8174691+kuruyia@users.noreply.github.com>

--- src/vmm/src/main.rs
+++ src/vmm/src/main.rs
@@ -1,6 +1,6 @@
 use crate::args::CliArguments;
 use clap::Parser;
-use tracing::{info, level_filters};
+use tracing::info;
 use vmm::core::{self, vmm::VMM};
 
 mod args;
@@ -16,16 +16,8 @@ pub enum Error {
 fn main() -> Result<(), Error> {
     // Parse the configuration and configure logger verbosity
     let args = CliArguments::parse();
-
     tracing_subscriber::fmt()
-        .with_max_level(match args.verbose.log_level_filter() {
-            log::LevelFilter::Off => level_filters::LevelFilter::OFF,
-            log::LevelFilter::Error => level_filters::LevelFilter::ERROR,
-            log::LevelFilter::Warn => level_filters::LevelFilter::WARN,
-            log::LevelFilter::Info => level_filters::LevelFilter::INFO,
-            log::LevelFilter::Debug => level_filters::LevelFilter::DEBUG,
-            log::LevelFilter::Trace => level_filters::LevelFilter::TRACE,
-        })
+        .with_max_level(args.convert_log_to_tracing())
         .init();
 
     info!(
