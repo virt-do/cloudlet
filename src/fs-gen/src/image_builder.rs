7eeec5466c267fda41887ccb1f0c6b1dc731e0cc
Mathias-Boulay <mathiasboulay@free.fr>
2024-03-30 17:43:14 +0100

    Update Cargo.toml, image_builder.rs, and main.rs
    Signed-off-by: BioTheWolff <47079795+BioTheWolff@users.noreply.github.com>

---
+++ src/fs-gen/src/image_builder.rs
@@ -0,0 +1,22 @@
+use std::path::{Path, PathBuf};
+
+use vfs::{FileSystem, OverlayFS, PhysicalFS, VfsPath};
+
+pub fn build_new_image(blob_paths: &Vec<PathBuf>, output_folder: &Path) {
+    let virtual_paths = blob_paths
+        .iter()
+        .map(|p| VfsPath::new(PhysicalFS::new(p)))
+        .collect::<Vec<VfsPath>>();
+
+    let vfs = OverlayFS::new(&virtual_paths);
+    let toto: Vec<String> = vfs.read_dir("/home").unwrap().collect();
+    println!("{:?}", toto);
+    println!("{:?}", vfs);
+    let overlay_root: VfsPath = vfs.into();
+
+    let output_vpath = VfsPath::new(PhysicalFS::new(output_folder));
+
+    overlay_root
+        .copy_dir(&output_vpath)
+        .expect("Failed to copy the blobs !");
+}
--- src/fs-gen/src/main.rs
+++ src/fs-gen/src/main.rs
@@ -1,10 +1,21 @@
+use std::{
+    path::{Path, PathBuf},
+    str::FromStr,
+};
+
 use clap::Parser;
 
-use crate::cli_args::CliArgs;
+use crate::{cli_args::CliArgs, image_builder::build_new_image};
 
 mod cli_args;
+mod image_builder;
 
 fn main() {
     let args = CliArgs::get_args();
     println!("Hello, world!, {:?}", args);
+
+    let paths: Vec<PathBuf> =
+        vec![PathBuf::from_str("../../image-gen/blobs/sha256/layer_1").unwrap()];
+
+    build_new_image(&paths, &PathBuf::from_str("./titi").unwrap());
 }
