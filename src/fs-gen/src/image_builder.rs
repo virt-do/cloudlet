0c55d220a1a6c4b4cf64af20ad63eae40adda178
Mathias-Boulay <mathiasboulay@free.fr>
2024-04-03 22:10:37 +0200

    wip: fuse overlayfs test
    Signed-off-by: BioTheWolff <47079795+BioTheWolff@users.noreply.github.com>

--- src/fs-gen/src/image_builder.rs
+++ src/fs-gen/src/image_builder.rs
@@ -2,21 +2,29 @@ use std::path::{Path, PathBuf};
 
 use vfs::{FileSystem, OverlayFS, PhysicalFS, VfsPath};
 
-pub fn build_new_image(blob_paths: &Vec<PathBuf>, output_folder: &Path) {
+pub fn build_new_image(blob_paths: &[PathBuf], output_folder: &Path) {
     let virtual_paths = blob_paths
         .iter()
         .map(|p| VfsPath::new(PhysicalFS::new(p)))
         .collect::<Vec<VfsPath>>();
 
     let vfs = OverlayFS::new(&virtual_paths);
-    let toto: Vec<String> = vfs.read_dir("/home").unwrap().collect();
+
+    let toto: Vec<String> = vfs.read_dir("").unwrap().collect();
+    // copy from the overlay fs to the output folder
+
     println!("{:?}", toto);
     println!("{:?}", vfs);
     let overlay_root: VfsPath = vfs.into();
 
-    let output_vpath = VfsPath::new(PhysicalFS::new(output_folder));
+    println!("{:?}", overlay_root);
+
+    let output_vpath = VfsPath::new(PhysicalFS::new("."));
+    println!("{:?}", output_vpath.as_str());
 
     overlay_root
-        .copy_dir(&output_vpath)
+        .join("bin")
+        .unwrap()
+        .copy_dir(&output_vpath.join("titi").unwrap())
         .expect("Failed to copy the blobs !");
 }
