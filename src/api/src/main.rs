6dec6b28ba6a42f19c56cc71b5d1cf28d28be5db
Thomas Mauran <78204354+thomas-mauran@users.noreply.github.com>
2024-04-03 20:06:54 +0200

    feat: add basic setup for the http api (#13)
    * feat: add basic setup for the http api
    
    Signed-off-by: Mauran <thomas.mauran@etu.umontpellier.fr>
    
    ---------
    
    Signed-off-by: Mauran <thomas.mauran@etu.umontpellier.fr>

---
+++ src/api/src/main.rs
@@ -0,0 +1,20 @@
+use actix_web::{App, HttpServer};
+use api::services::{configuration, logs, metrics, run, shutdown};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let port = 3000;
+
+    println!("Starting server on port:  {}", port);
+    HttpServer::new(|| {
+        App::new()
+            .service(configuration)
+            .service(run)
+            .service(logs)
+            .service(metrics)
+            .service(shutdown)
+    })
+    .bind(("127.0.0.1", port))?
+    .run()
+    .await
+}
