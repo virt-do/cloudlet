27bf4352f0346d42eb3eaae14840740d9a58530e
Mathias-Boulay <mathiasboulay@free.fr>
2024-04-17 22:39:20 +0200

    cut: remove unused stuff
    Signed-off-by: Mathias-Boulay <mathiasboulay@free.fr>

--- src/fs-gen/src/main.rs
+++ src/fs-gen/src/main.rs
@@ -1,20 +1,12 @@
 //! Main module for the initramfs tarball generation
 
-use std::{
-    path::PathBuf,
-    str::FromStr,
-};
-use clap::Parser;
 use crate::{cli_args::CliArgs, image_builder::build_new_image};
+use clap::Parser;
+use std::{path::PathBuf, str::FromStr};
 
 mod cli_args;
 mod image_builder;
 
 fn main() {
     let args = CliArgs::get_args();
-
-    let paths: Vec<PathBuf> =
-        vec![PathBuf::from_str("../../image-gen/blobs/sha256/layer_1").unwrap()];
-
-    build_new_image(&paths, &PathBuf::from_str("./titi").unwrap());
 }
