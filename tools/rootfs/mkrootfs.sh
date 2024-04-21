6e7e4d9bb37eb27942b20e891cc64f0aafc6d41e
Martin Moreira de Jesus <martin.moreira-de-jesus@protonmail.com>
2024-04-21 22:16:49 +0200

    make(tools/rootfs): setup slip interface
    Signed-off-by: Martin Moreira de Jesus <martin.moreira-de-jesus@protonmail.com>

--- tools/rootfs/mkrootfs.sh
+++ tools/rootfs/mkrootfs.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/bash
 
-curl -O https://dl-cdn.alpinelinux.org/alpine/v3.14/releases/x86_64/alpine-minirootfs-3.14.2-x86_64.tar.gz
+if [ ! -d alpine-minirootfs ]
+then
+    curl -O https://dl-cdn.alpinelinux.org/alpine/v3.14/releases/x86_64/alpine-minirootfs-3.14.2-x86_64.tar.gz
 
-mkdir alpine-minirootfs
-tar xf alpine-minirootfs-3.14.2-x86_64.tar.gz -C alpine-minirootfs
+    mkdir alpine-minirootfs
+    tar xf alpine-minirootfs-3.14.2-x86_64.tar.gz -C alpine-minirootfs
+fi
 
 pushd alpine-minirootfs
 cat > init <<EOF
@@ -14,7 +17,16 @@ cat > init <<EOF
 mount -t devtmpfs dev /dev
 mount -t proc proc /proc
 mount -t sysfs sysfs /sys
+
 ip link set up dev lo
+
+slattach -L /dev/ttyS1&
+
+while ! ifconfig sl0 &> /dev/null; do
+    sleep 1
+done
+
+ifconfig sl0 172.30.0.11 netmask 255.255.0.0 up
 
 exec /sbin/getty -n -l /bin/sh 115200 /dev/console
 poweroff -f
