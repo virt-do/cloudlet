b7221d9b99f8f55cea5990ffd38dfd3dd7268abd
Martin Moreira de Jesus <martin.moreira-de-jesus@protonmail.com>
2024-04-23 18:18:07 +0200

    feat(agent/grpc): add service skeleton
    Signed-off-by: Martin Moreira de Jesus <martin.moreira-de-jesus@protonmail.com>

--- src/agent/src/workload/service.rs
+++ src/agent/src/workload/service.rs
@@ -1,1 +1,23 @@
+use crate::agent::{self, ExecuteRequest, ExecuteResponse, SignalRequest};
 
+use agent::workload_runner_server::WorkloadRunner;
+
+use tokio_stream::wrappers::ReceiverStream;
+use tonic::{Request, Response};
+
+type Result<T> = std::result::Result<Response<T>, tonic::Status>;
+
+pub struct WorkloadRunnerService {}
+
+#[tonic::async_trait]
+impl WorkloadRunner for WorkloadRunnerService {
+    type ExecuteStream = ReceiverStream<std::result::Result<ExecuteResponse, tonic::Status>>;
+
+    async fn execute(&self, _: Request<ExecuteRequest>) -> Result<Self::ExecuteStream> {
+        unreachable!("Not implemented")
+    }
+
+    async fn signal(&self, _: Request<SignalRequest>) -> Result<()> {
+        unreachable!("Not implemented")
+    }
+}
