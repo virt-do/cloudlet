5a1dc2492ae16eae69cd52caa20bff611394e036
BioTheWolff <47079795+BioTheWolff@users.noreply.github.com>
2024-04-24 09:35:19 +0200

    refactor: implement better error handling for initramfs generation
    Signed-off-by: BioTheWolff <47079795+BioTheWolff@users.noreply.github.com>

--- src/fs-gen/src/initramfs_generator.rs
+++ src/fs-gen/src/initramfs_generator.rs
@@ -4,44 +4,56 @@ use std::os::unix::fs::PermissionsExt;
 use std::path::{Path, PathBuf};
 use std::process::{Command, Stdio};
 use tracing::info;
+use anyhow::{Context, Result};
 
 const INIT_FILE: &str = include_str!("../resources/initfile");
 
-pub fn create_init_file(path: &Path, initfile: Option<PathBuf>) {
+pub fn create_init_file(path: &Path, initfile: Option<PathBuf>) -> Result<()> {
     info!("Writing initfile...");
 
     let destination = path.join("init");
 
     if let Some(p) = initfile {
         // if there is a given initfile, we copy it into the folder
-        fscopy(p, destination).expect("Could not copy initfile");
+        fscopy(p, destination)
+            .with_context(|| "Failed to copy provided initfile to initramfs".to_string())?;
     } else {
         // if there is none, write the default init file
         let mut file = File::create(destination).unwrap();
         file.set_permissions(Permissions::from_mode(0o755)).unwrap();
+
         file.write_all(INIT_FILE.as_bytes())
-            .expect("Could not write init file");
+            .with_context(|| "Failed to write default initfile to initramfs".to_string())?;
     }
 
     info!("Initfile written!");
+
+    Ok(())
 }
 
-pub fn insert_agent(destination: &Path, agent_path: PathBuf) {
+pub fn insert_agent(destination: &Path, agent_path: PathBuf) -> Result<()> {
     info!("Inserting agent into fs...");
 
-    let mut file = File::create(destination.join("agent")).unwrap();
-    file.set_permissions(Permissions::from_mode(0o755)).unwrap();
+    let mut file = File::create(destination.join("agent"))
+        .with_context(|| "Could not open agent file inside initramfs".to_string())?;
+    file.set_permissions(Permissions::from_mode(0o755))
+        .with_context(|| "Failed to set permissions for agent file".to_string())?;
 
-    let mut agent = File::open(agent_path).unwrap();
-    iocopy(&mut agent, &mut file).expect("Could not copy agent");
+    let mut agent = File::open(agent_path)
+        .with_context(|| "Could not open host agent file".to_string())?;
+    iocopy(&mut agent, &mut file)
+        .with_context(|| "Failed to copy agent contents from host to destination".to_string())?;
 
     info!("Agent inserted!");
+
+    Ok(())
 }
 
-pub fn generate_initramfs(root_directory: &Path, output: &Path) {
-    let file = File::create(output).unwrap();
+pub fn generate_initramfs(root_directory: &Path, output: &Path) -> Result<()> {
+    let file = File::create(output)
+        .with_context(|| "Could not open output file to write initramfs".to_string())?;
     file.set_permissions(Permissions::from_mode(0o644))
-        .expect("Could not set permissions");
+        .with_context(|| "Failed to set permissions for output file".to_string())?;
 
     info!("Generating initramfs...");
 
@@ -51,10 +63,13 @@ pub fn generate_initramfs(root_directory: &Path, output: &Path) {
         .arg("-c")
         .arg("find . -print0 | cpio -0 --create --owner=root:root --format=newc | xz -9 --format=lzma")
         .spawn()
-        .expect("Failed to package initramfs");
+        .with_context(|| "Failed to package initramfs into bundle".to_string())?;
+
     command
         .wait()
-        .expect("Failed to wait for initramfs to finish");
+        .with_context(|| "Encountered exception while waiting for bundling to finish".to_string())?;
 
     info!("Initramfs generated!");
+
+    Ok(())
 }
