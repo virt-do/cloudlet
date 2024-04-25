b6c0ea169e3d47316ab21b65d17dd6c8424edb49
Martin Moreira de Jesus <martin.moreira-de-jesus@protonmail.com>
2024-04-25 22:32:47 +0200

    fix(tools/rootfs): bad path for file copy
    Signed-off-by: Martin Moreira de Jesus <martin.moreira-de-jesus@protonmail.com>

--- tools/rootfs/mkrootfs.sh
+++ tools/rootfs/mkrootfs.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+set -e
+
 if [ ! -d alpine-minirootfs ]
 then
     curl -O https://dl-cdn.alpinelinux.org/alpine/v3.14/releases/x86_64/alpine-minirootfs-3.14.2-x86_64.tar.gz
@@ -8,13 +10,12 @@ then
     tar xf alpine-minirootfs-3.14.2-x86_64.tar.gz -C alpine-minirootfs
 fi
 
-cp ../../target/x86_64-unknown-linux-musl/release/agent alpine-minirootfs/agent
-
-mkdir -p alpine-minirootfs/etc/cloudlet/agent
-
-cp ../../src/agent/examples/config.toml etc/cloudlet/agent/config.toml
 
 pushd alpine-minirootfs
+mkdir -p etc/cloudlet/agent
+cp ../../../target/x86_64-unknown-linux-musl/release/agent agent
+cp ../config.toml etc/cloudlet/agent/config.toml
+
 cat > init <<EOF
 #! /bin/sh
 #
